4b58621ba8a52a074fba4660daa2b4791a068917
Chris Reske <chris.reske@outlook.de>
2022-10-14 10:19:07 +0200

    Add Dto: 'BookCreateDto'

---
+++ BookStoreApp.API/Models/Book/BookCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApp.API.Models.Book;
+
+public class BookCreateDto
+{
+    [Required]
+    [StringLength(100)]
+    public string Title { get; set; }
+
+    [Required] 
+    public int Year { get; set; }
+
+    [Required] public string Isbn { get; set; }
+
+    [Required]
+    [StringLength (500, MinimumLength =10)]
+    public string Summary { get; set; }
+
+    public string Image { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue)]
+    public decimal Price { get; set; }
+}
