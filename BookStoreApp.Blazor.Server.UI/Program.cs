121bd3e17914f5afdc4978e766116b68a0027bc6
Chris Reske <chris.reske@outlook.de>
2023-02-16 16:30:43 +0100

    Setup BookService interface and implementation. Add reference to Program.cs. Adjust namespaces.

--- BookStoreApp.Blazor.Server.UI/Program.cs
+++ BookStoreApp.Blazor.Server.UI/Program.cs
@@ -1,7 +1,6 @@
 using Blazored.LocalStorage;
 using BookStoreApp.Blazor.Server.UI.Configurations;
 using BookStoreApp.Blazor.Server.UI.Providers;
-using BookStoreApp.Blazor.Server.UI.Services;
 using BookStoreApp.Blazor.Server.UI.Services.Authentication;
 using BookStoreApp.Blazor.Server.UI.Services.Author;
 using BookStoreApp.Blazor.Server.UI.Services.Base;
@@ -22,6 +21,7 @@ builder.Services.AddHttpClient<IClient, Client>(cl =>
 
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddAutoMapper(typeof(MapperConfig));
 builder.Services.AddScoped<ApiAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(p => 
---
+++ BookStoreApp.Blazor.Server.UI/Services/Author/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using BookStoreApp.Blazor.Server.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Author;
+
+public interface IBookService
+{
+    // Todo: Make Serivce(s) and methods generic (at a later stage)
+    Task<Response<List<BookReadOnlyDto>>> Get();
+    Task<Response<BookDetailsDto>> Get(int id);
+    Task<Response<BookUpdateDto>> GetForUpdate(int id);
+    Task<Response<int>> Create(BookCreateDto author);
+    Task<Response<int>> Edit(int id, BookUpdateDto author);
+    Task<Response<int>> Delete(int id);
+}
